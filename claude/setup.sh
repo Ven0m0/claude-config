391820a91d9e0990c4c8339f94729d00be035f1a
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-03-03 13:02:46 +0100

    Update setup.sh

--- claude/setup.sh
+++ claude/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Mark the current repository as safe for Git to prevent "dubious ownership" errors,
-# which can occur in containerized environments when directory ownership doesn't match the current user
+# Make git comnands faster and less verbose to save tokens
 git config --global --add safe.directory "$(realpath .)"
 git config --global index.version 4
 git config --global index.threads 0
@@ -9,6 +8,7 @@ git config --global http.version "HTTP/2"
 git config --global protocol.version 2
 git config --global core.compression 9
 git config --global core.preloadindex true
+git config --global diff.context 3
 git config --global diff.suppressBlankEmpty true
 git config --global diff.compactionHeuristic true
 git config --global diff.ignoreSubmodules true
@@ -18,6 +18,7 @@ git config --global fetch.parallel 0
 git config --global pack.threads 0
 git config --global status.short true
 git config --global commit.verbose false
+git config --global feature.manyFiles true
 
 command -v uv || wget -qO- https://astral.sh/uv/install.sh | sh
 
