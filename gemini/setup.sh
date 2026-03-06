c2f424a6137d894cfef0437938df2fe88ef69e7a
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-03-06 15:17:52 +0100

    Create setup.sh

---
+++ gemini/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gemini extensions install https://github.com/gemini-cli-extensions/conductor --auto-update
+gemini extensions install https://github.com/gemini-cli-extensions/code-review
+gemini extensions install https://github.com/gemini-cli-extensions/jules --auto-update
