2698b8246ba0c7ed2a9280d6d5e282570ed2a0db
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-03-09 04:07:55 +0100

    Update setup.sh

--- gemini/setup.sh
+++ gemini/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-gemini extensions install https://github.com/gemini-cli-extensions/conductor --auto-update
-gemini extensions install https://github.com/gemini-cli-extensions/code-review
-gemini extensions install https://github.com/gemini-cli-extensions/jules --auto-update
+gemini extensions install https://github.com/gemini-cli-extensions/conductor --auto-update --consent --pre-release
+gemini extensions install https://github.com/gemini-cli-extensions/code-review --auto-update --consent --pre-release
+gemini extensions install https://github.com/gemini-cli-extensions/jules --auto-update --consent --pre-release
 
-gemini extensions install https://github.com/gemini-cli-extensions/ralph --auto-update
-gemini extensions install https://github.com/gemini-cli-extensions/mcp-toolbox
+gemini extensions install https://github.com/gemini-cli-extensions/ralph --auto-update --consent --pre-release
+gemini extensions install https://github.com/gemini-cli-extensions/mcp-toolbox --auto-update --consent --pre-release
+
+gemini extensions update --all
