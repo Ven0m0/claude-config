4d0a6162c6520fa782dc1c3dd9f05ce64982937f
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-03-09 10:02:30 +0100

    Create setup.sh

---
+++ copilot-cli/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+copilot plugin install context-engineering@awesome-copilot
+copilot plugin install frontend-web-dev@awesome-copilot
+copilot plugin install project-planning@awesome-copilot
+copilot plugin install rug-agentic-workflow@awesome-copilot
+copilot plugin install spark@copilot-plugins
